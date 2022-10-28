ddcd72feb4642356bb2b3d45a935d5ee55b756ee
Vanessa Vasquez <vvasquez528@gmail.com>
2022-10-28 00:51:55 -0400

    Add basic flow
    - Need to work on hashtag and summary page

--- server.py
+++ server.py
@@ -1,9 +1,60 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, render_template, request, make_response
+import openai 
+import re
+
+openai.api_key ="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"
 
 app = Flask(__name__)
+
+def get_captions():
+  prompt = f"List 5 captions for {topic} to post on social media. Make sure that these captions are {tone} and fits someone who identifies as an {identity}."
+  completion = openai.Completion.create(engine="text-davinci-002", max_tokens=256, prompt=prompt)
+  result = completion.choices[0].text.strip()
+  ideas = re.split('\n', completion.choices[0].text.strip())
+  return result, ideas
+
+def get_hashtags():
+  prompt = f"List 5 hashtags for {topic} to post on social media. Make sure that these captions are {tone} and fits someone who identifies as an {identity}."
+  completion = openai.Completion.create(engine="text-davinci-002", max_tokens=256, prompt=prompt)
+  result = completion.choices[0].text.strip()
+  ideas = re.split('\n', completion.choices[0].text.strip())
+  return result, ideas
 
 @app.route("/")
 def index():
     return render_template("questionnaire.html")
+
+@app.route("/inputs", methods=['GET', 'POST'])
+def process_input():
+    json_data = request.get_json()
+
+    description = int(json_data["description"])
+    user_input = json_data["input"]
+
+    response_data = {'code': 'SUCCESS'}
+
+    if description == 0:
+        global identity
+        identity = user_input
+    elif description == 1:
+        global tone
+        tone = user_input
+    elif description == 2:
+        global topic
+        topic = user_input
+        captions, ideas = get_captions()
+        response_data = {'captions': ideas}
+    elif description == 3:
+        hashtags, ideas = get_hashtags()
+        response_data = {'hashtags': ideas}
+    
+    return jsonify(response_data)
+
+
+
+
+
+
+
+    
 
--- static/index.js
+++ static/index.js
@@ -1,27 +1,96 @@
 const descriptions = {
   0: "To get started, tell us a little about who you are. <br/> Are you an influencer, a salesman, etc?",
-  1: "Tell us about the tone of your message. Do you want to be trendy, professional, sarcastic, geeky, etc?",
+  1: "Tell us about the tone of your message. <br/> Do you want to be trendy, professional, sarcastic, geeky, etc?",
   2: "Cool! Now, tell us a little bit more about the product you want to post about. What are you selling? What can people do with it?",
-  3: "Lets take a look at some captions based on your input:",
+  3: "Lets take a look at some captions based on your input: <br/>",
   4: "Here are some hashtags to pair your caption with: ",
 };
 
 let currDescIdx = 0;
 
-const renderDescription = () => {
+let captionsArr;
+let hashtagsArr;
+
+const renderContent = async (userInput) => {
   $(".description").html(descriptions[currDescIdx]);
+
+  if (currDescIdx === 2) {
+    try {
+      const response = await axios.post("/inputs", {
+        description: currDescIdx,
+        input: userInput,
+      });
+
+      captionsArr = response["data"]["captions"];
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (currDescIdx === 3) {
+    $("#submit-btn").addClass("d-none");
+    $("#like").removeClass("d-none");
+    $("#regenerate").removeClass("d-none");
+
+    try {
+      const response = await axios.post("/inputs", {
+        description: currDescIdx,
+        input: userInput,
+      });
+
+      hashtagsArr = response["data"]["hashtags"];
+
+      captionsArr.forEach((caption) => {
+        $(".description").append(caption + "</br>");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (currDescIdx === 4) {
+    hashtagsArr.forEach((hashtag) => {
+      $(".description").append(hashtag);
+    });
+  }
 };
 
 const handleSubmit = () => {
   $("#submit-btn").click(() => {
+    let userInput = $("#user-input").val();
+
+    axios
+      .post("/inputs", {
+        description: currDescIdx,
+        input: userInput,
+      })
+      .then(function (response) {
+        console.log("printing", response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
     currDescIdx++;
 
-    renderDescription();
+    renderContent(userInput);
+
+    $("#user-input").val(" ");
+  });
+};
+
+const handleRegenerate = () => {
+  $("#regenerate").on("click", () => {});
+};
+
+const handleLike = () => {
+  $("#like").on("click", () => {
+    currDescIdx++;
   });
 };
 
 $(document).ready(() => {
-  renderDescription();
+  renderContent();
 
   handleSubmit();
+
+  handleRegenerate();
+
+  handleLike();
 });
