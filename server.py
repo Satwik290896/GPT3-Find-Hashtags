4c5d1d0f7de27b4ac9aca4188795a66a052d6dce
ktkay <kt2593@columbia.edu>
2022-10-27 22:26:59 -0400

    added caption.html and openai to server.py

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from flask import render_template
+import os
+import openai
+import ipywidgets as widgets
+import textwrap as tw
+import re
+import json 
+import config
+
+# load any file in .env
+openai.api_key = config.api_key
 
 app = Flask(__name__)
 
@@ -7,5 +17,13 @@ app = Flask(__name__)
 def index():
     return render_template("questionnaire.html")
 
+@app.route("/caption")
+def captionGenerator():
+    prompt = f"List 5 captions for cookies cutter to post on Instagram"
+    completion = openai.Completion.create(engine="text-davinci-002", max_tokens=256, prompt=prompt)
+    result = completion.choices[0].text.strip()
+    ideas = re.split("\n", completion.choices[0].text.strip())
+   
+    return render_template("caption.html", prompt=prompt, ideas=result)
 if __name__ == '__main__':
     app.run(debug = True)
